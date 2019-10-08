ad0d944ce1ea207f41ac466c08f68c868d232f4d
Sean Behan <codebam@riseup.net>
2019-10-07 20:55:24 -0400

    Simplify

--- client.py
+++ client.py
@@ -4,26 +4,19 @@ import json
 CLIENT_VERSION = 1
 
 
-class Connection:
-    def __init__(self, reader: asyncio.StreamReader, writer: asyncio.StreamWriter):
-        self.reader = reader
-        self.writer = writer
-
-
 class Client:
     def __init__(self, username: str):
         self.username = username
 
     async def connect(self):
-        reader, writer = await asyncio.open_connection("127.0.0.1", 8080)
-        self.conn = Connection(reader, writer)
+        self.reader, self.writer = await asyncio.open_connection("127.0.0.1", 8080)
 
     async def login(self):
-        if self.conn is not None:
+        if self.writer is not None:
             r = json.dumps(
                 {"version": CLIENT_VERSION, "from": self.username, "verb": "LOGN"}
             )
-            self.conn.writer.write(r.encode())
+            self.writer.write(r.encode())
         else:
             raise ConnectionError("must connect before logging in")
 
