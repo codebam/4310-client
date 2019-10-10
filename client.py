32297213a61dbc8470a7d68c59ba86cff35f97b9
Sean Behan <codebam@riseup.net>
2019-10-10 18:54:13 -0400

    Fix message reciever and sender

--- client.py
+++ client.py
@@ -1,51 +1,115 @@
-import asyncio
+import trio
 import json
 import pprint
 
-CLIENT_VERSION = 1
+_CLIENT_VERSION = 1
+_RECEIVE_SIZE = 4096  # pretty arbitrary
+
+
+class TerminatedFrameReceiver:
+    def __init__(self, stream, terminator, max_frame_length=16384):
+        self.stream = stream
+        self.terminator = terminator
+        self.max_frame_length = max_frame_length
+        self._buf = bytearray()
+        self._next_find_idx = 0
+
+    async def receive(self):
+        while True:
+            terminator_idx = self._buf.find(self.terminator, self._next_find_idx)
+
+            if terminator_idx < 0:
+                # no terminator found
+
+                if len(self._buf) > self.max_frame_length:
+                    raise ValueError("frame too long")
+                # next time, start the search where this one left off
+                self._next_find_idx = max(0, len(self._buf) - len(self.terminator) + 1)
+                # add some more data, then loop around
+                more_data = await self.stream.receive_some(_RECEIVE_SIZE)
+
+                if more_data == b"":
+                    if self._buf:
+                        raise ValueError("incomplete frame")
+                    raise trio.EndOfChannel
+                self._buf += more_data
+            else:
+                # terminator found in buf, so extract the frame
+                frame = self._buf[:terminator_idx]
+                # Update the buffer in place, to take advantage of bytearray's
+                # optimized delete-from-beginning feature.
+                del self._buf[: terminator_idx + len(self.terminator)]
+                # next time, start the search from the beginning
+                self._next_find_idx = 0
+
+                return frame
+
+    def __aiter__(self):
+        return self
+
+    async def __anext__(self):
+        try:
+            return await self.receive()
+        except trio.EndOfChannel:
+            raise StopAsyncIteration
+
+
+class Packet:
+    def __init__(
+        self,
+        version=_CLIENT_VERSION,
+        number=None,
+        _from=None,
+        to=None,
+        verb=None,
+        data=None,
+    ):
+        self.__json_packet = json.dumps(
+            {
+                "version": version,
+                "number": number,
+                "from": _from,
+                "to": to,
+                "verb": verb,
+                "data": data,
+            }
+        )
+
+    def encode(self) -> bytes:
+        return self.__json_packet.encode() + b"\n"
 
 
 class Client:
     def __init__(self, username: str):
         self.username = username
 
-    async def connect(self):
-        print("connecting...")
-        self.reader, self.writer = await asyncio.open_connection("127.0.0.1", 8080)
-        print("connected!")
+    async def run(self, host="127.0.0.1", port=8080):
+        self.__stream = await trio.open_tcp_stream(host, port)
 
-    async def login(self):
-        print("logging in...")
+        async with trio.open_nursery() as nursery:
+            nursery.start_soon(self.__receiver)
+            nursery.start_soon(self.__sender)
 
-        if self.writer is not None:
-            r = json.dumps(
-                {"version": CLIENT_VERSION, "from": self.username, "verb": "LOGN"}
-            )
-            self.writer.write(r.encode())
-        else:
-            raise ConnectionError("must connect before logging in")
-        print("logged in!")
+    async def __logn(self):
+        p = Packet(_from=self.username, verb="LOGN")
+        await self.__stream.send_all(p.encode())
+        # d = """{ "version": 1, "from": "codebam", "verb": "LOGN" }\n"""
+        # await self.__stream.send_all(bytes(d, "utf8"))
 
-    async def read_packets(self):
-        line = await self.reader.readline()
+    async def __sender(self):
+        await self.__logn()
+        # await self.__stream.aclose()
 
-        while line is not None:
-            self.handle_packet(line)
-
-    async def handle_packet(self, line):
-        packet = json.dumps(json.loads(line))
-        pp = pprint.PrettyPrinter(indent=4)
-        pp.pprint(packet)
-        # TODO
+    async def __receiver(self):
+        chan = TerminatedFrameReceiver(self.__stream, b"\n")
+        async for message in chan:
+            print(f"Got message: {message!r}")
 
 
 async def main():
-    c = Client(username=input("username: "))
-    await c.connect()
-    await c.login()
-    await c.read_packets()
-    asyncio.run(c.read_packets())
+    client = Client(username=input("enter your username: ").strip())
+    await client.run()
 
 
 if __name__ == "__main__":
-    asyncio.run(main())
+    trio.run(main)
